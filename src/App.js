aba96eef19c5203295582e44719d65e2231c552a
Basirul Billah <basirulbillah@gmail.com>
2022-05-24 02:17:22 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div>
-      
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}/>
+        <Route path="/home" element={<Home></Home>}/>
+      </Routes>
     </div>
   );
 }
