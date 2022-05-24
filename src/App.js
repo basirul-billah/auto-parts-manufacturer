6f62148fc30ac2c1cf207ce6b366905bb7ce6eda
Basirul Billah <basirulbillah@gmail.com>
2022-05-24 17:38:56 +0600

    added footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 
 function App() {
   return (
-    <div>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Home></Home>}/>
-        <Route path="/home" element={<Home></Home>}/>
-        <Route path="/login" element={<Login></Login>}/>
-      </Routes>
+    <div className="relative">
+      <div className="min-h-screen">
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/home" element={<Home></Home>} />
+          <Route path="/login" element={<Login></Login>} />
+        </Routes>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center p-4 bg-neutral text-neutral-content absolute">
+            <div>
+                <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
