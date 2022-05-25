ba47e21d98b8a8a166bee3c25f94d9d3b70359ac
Basirul Billah <basirulbillah@gmail.com>
2022-05-26 00:29:04 +0600

    added require auth

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import RequireAuth from './Pages/Login/RequireAuth';
 import SignUp from './Pages/Login/SignUp';
 import Purchase from './Pages/Purchase/Purchase';
 
@@ -15,7 +16,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home></Home>} />
           <Route path="/home" element={<Home></Home>} />
-          <Route path="/products/:productId" element={<Purchase></Purchase>}/>
+          <Route path="/products/:productId" element={
+            <RequireAuth>
+              <Purchase></Purchase>
+            </RequireAuth>
+          }/>
           <Route path="/login" element={<Login></Login>} />
           <Route path="/signup" element={<SignUp></SignUp>} />
         </Routes>
